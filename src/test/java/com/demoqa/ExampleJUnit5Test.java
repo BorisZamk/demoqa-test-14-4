0fb8ec3bf749c52a3ff5c6e4dfc4cbc329dc46d1
Boris Zamkovoy <bzamkovoy@gmail.com>
2022-09-13 18:13:11 +0300

    added ExampleJUnitTest class

---
+++ src/test/java/com/demoqa/ExampleJUnit5Test.java
@@ -0,0 +1,12 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleJUnit5Test {
+
+    @Test
+    void firsTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
